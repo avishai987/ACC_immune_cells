fa4ba6348d93e9d453a4929ae8c60c160ecb3440
avishai987 <avishai987@gmail.com>
2025-09-29 10:53:08 +0300

    add

---
+++ envs/install_cran_packages.R
@@ -0,0 +1,4 @@
+install.packages("MakefileR")
+devtools::install_github("avishai987/print.rmd.tabs")
+devtools::install_github("avishai987/SourceFromGithub")
+devtools::install_github('zktuong/ktplots@1bd30b5', dependencies = F)

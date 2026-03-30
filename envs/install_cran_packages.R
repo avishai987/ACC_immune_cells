c6d6487cb2ab755ee8599cce2a5d40fa89ec1b16
avishai987 <avishai987@gmail.com>
2026-03-30 13:52:58 +0300

    add packages

--- envs/install_cran_packages.R
+++ envs/install_cran_packages.R
@@ -3,3 +3,5 @@ devtools::install_github("avishai987/print.rmd.tabs")
 devtools::install_github("avishai987/SourceFromGithub")
 devtools::install_github('zktuong/ktplots@1bd30b5', dependencies = F)
 devtools::install_github("montilab/hypeR@e407bf1", dependencies = F) # cannot install with conda due to https://github.com/montilab/hypeR/issues/58
+remotes::install_github("saezlab/liana@6cab46c",  upgrade = "never")
+pak::pkg_install("saeyslab/nichenetr@2d5c1ab", upgrade = FALSE,ask = T) # run from terminal

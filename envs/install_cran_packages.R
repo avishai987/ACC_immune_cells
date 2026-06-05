3e20ca2b9b48e9052330d1de6065664e36696576
avishai987 <avishai987@gmail.com>
2026-06-05 14:37:25 +0300

    add gdiff

--- envs/install_cran_packages.R
+++ envs/install_cran_packages.R
@@ -5,3 +5,6 @@ devtools::install_github('zktuong/ktplots@1bd30b5', dependencies = F)
 devtools::install_github("montilab/hypeR@e407bf1", dependencies = F) # cannot install with conda due to https://github.com/montilab/hypeR/issues/58
 remotes::install_github("saezlab/liana@6cab46c",  upgrade = "never")
 pak::pkg_install("saeyslab/nichenetr@2d5c1ab", upgrade = FALSE,ask = T) # run from terminal
+pak::pkg_install("gdiff", upgrade = FALSE,ask = T) # run from terminal
+
+
